dedee96c4a3ac19fa32620cf7296e1be236bcd5c
Marcos <lavarellomarcos@gmail.com>
2024-10-24 21:05:54 -0300

    fix: test

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,8 @@ import './App.css';
 function App() {
   const [count, setCount] = useState(0);
 
-  const prueba = '';
+  const prueba = 'a';
+
   console.log(prueba);
 
   return (
@@ -15,11 +16,13 @@ function App() {
         <a href="https://vitejs.dev" target="blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
+
         <a href="https://react.dev" target="blank">
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
       <h1>Vite + React</h1>
+
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
